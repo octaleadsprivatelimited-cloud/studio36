c93a975b2b61e0f0a74af5fbf1d6875f5ec212f1
Octa Leads <octaleadsprivatelimited@gmail.com>
2025-10-11 16:23:32 +0530

    Fix mobile menu: prevent hero section movement and body scroll when menu opens

--- js/script.js
+++ js/script.js
@@ -2,6 +2,33 @@ $(function () {
     $('.nav-btn').on('click', function () {
         $(this).toggleClass('open');
     });
+
+    // Handle mobile menu toggle to prevent body scroll and movement
+    const navbarToggler = $('.navbar-toggler');
+    const navbarCollapse = $('.navbar-collapse');
+    
+    navbarToggler.on('click', function() {
+        // Toggle menu-open class on body when menu opens/closes
+        setTimeout(function() {
+            if (navbarCollapse.hasClass('show') || navbarCollapse.hasClass('collapsing')) {
+                $('body').addClass('menu-open');
+            } else {
+                $('body').removeClass('menu-open');
+            }
+        }, 50);
+    });
+
+    // Remove menu-open class when clicking on nav links
+    $('.navbar-nav a').on('click', function() {
+        $('body').removeClass('menu-open');
+    });
+
+    // Remove menu-open class when clicking outside the menu
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.navbar').length && navbarCollapse.hasClass('show')) {
+            $('body').removeClass('menu-open');
+        }
+    });
 });
 
 (function () {
